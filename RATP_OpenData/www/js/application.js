c1af57a852d620712dda2e798f776cf1df169593
Hernandez Julien <julienhernandez14@gmail.com>
2017-06-19 11:20:15 +0200

    Recupération des infos sur les transports des stations + correction des appellent des API navitia

--- RATP_OpenData/www/js/application.js
+++ RATP_OpenData/www/js/application.js
@@ -9,65 +9,15 @@ class Application {
      */
     constructor() {
         this._serverPhonegap = location.href.split(':')[1].substr(2);
-        this._distance = "50";
 
         // powered by the server
         this._Host = 'http://134.157.46.190:3000/';
-        this._APINativia = 'https://9a515a8c-7b22-456e-8e0d-6bdddfd9206f@api.navitia.io/v1/coverage/fr-idf/';
 
         // views
         this._test = new TestView();
 
         console.log("++++++++++", this._serverPhonegap);
 
-    }
-
-    /**
-     * Get API Nativia pour les stations à proximité
-     */
-    callAPICloseTo(coords) {
-        $.ajax({
-            url: this._APINativia + "coords/"+coords+"/lines?distance="+this._distance,
-        }).done($.proxy(function(data){
-
-            let res = data;
-            console.log('getAPIcloseTo response', res);
-
-        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
-            console.log('CALL API CLOSETO FAILED: ', jqXHR, textStatus, errorThrown);
-        });
-    }
-
-    /**
-     * Get API Nativia pour les horaires des transports à la station recherché
-     */
-    callAPIStation(codeStation, dateTime) {
-        $.ajax({
-            url: this._APINativia + "stop_areas/stop_area"+codeStation+"/arrivals?from_datetime="+dateTime,
-        }).done($.proxy(function(data){
-
-            let res = data;
-            console.log('getAPIstation response', res);
-
-        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
-            console.log('CALL API STATION FAILED: ', jqXHR, textStatus, errorThrown);
-        });
-    }
-
-    /**
-     * Get API Nativia pour recherche d'itinéraire
-     */
-    callAPIItineraire(codeStationFrom, codeStationTo, dateTime) {
-        $.ajax({
-            url: this._APINativia + "journeys?from=stop_area"+codeStationFrom+"&to=stop_area"+codeStationTo+"&datetime="+dateTime,
-        }).done($.proxy(function(data){
-
-            let res = data;
-            console.log('getAPIitinearaire response', res);
-
-        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
-            console.log('CALL API ITINERAIRE FAILED: ', jqXHR, textStatus, errorThrown);
-        });
     }
 
     /**
--- RATP_OpenData/www/js/test.js
+++ RATP_OpenData/www/js/test.js
@@ -6,15 +6,17 @@ class TestView {
 
     constructor() {
         console.log("=============== Test::constructor() ========== ");
+        this._APINativia = 'https://9a515a8c-7b22-456e-8e0d-6bdddfd9206f@api.navitia.io/v1/coverage/fr-idf/';
+        this._distance = "50";
     }
 
-    init(server) {
+    init() {
         console.log("=============== Test::init() ========== ");
-        console.log(server);
+        console.log();
 
-        this.dataAPICloseTo("2.37768;48.85334");
-        this.dataAPIStation("%3AOIF%3ASA%3A59300","20170615T164106&");
-        this.dataAPIItineraire("%3AOIF%3ASA%3A59300","%3AOIF%3ASA%3A59522","20170615T164106&")
+        this.callAPICloseTo("2.37768;48.85334");
+        this.callAPIStation("%3AOIF%3ASA%3A59300","20170615T164106&");
+        this.callAPIItineraire("%3AOIF%3ASA%3A59300","%3AOIF%3ASA%3A59522","20170615T164106&")
 
     }
 
@@ -22,31 +24,47 @@ class TestView {
      * Fonction qui appelle l'API nativia pour récupérer les datas des stations à proximité
      * @param coords
      */
-    dataAPICloseTo(coords) {
-        app.callAPICloseTo(coords, $.proxy(function(data) {
-            if (data.error) {
-                console.log('dataAPICloseTo ERROR', data);
-            }
-            else {
-                console.log('dataAPICloseTo response', data);
-            }
-        }, this));
+    callAPICloseTo(coords) {
+        $.ajax({
+            url: this._APINativia + "coords/"+coords+"/lines?distance="+this._distance,
+        }).done($.proxy(function(data){
+
+            let res = data;
+            console.log('getAPIcloseTo response', res);
+
+        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
+            console.log('CALL API CLOSETO FAILED: ', jqXHR, textStatus, errorThrown);
+        });
     }
+
 
     /**
      * Fonction qui appelle l'API navitia pour récupérer les passages des prochains transports à un point donné
      * @param codeStation
      * @param dateTime
      */
-    dataAPIStation(codeStation, dateTime) {
-        app.callAPIStation(codeStation,dateTime, $.proxy(function(data) {
-            if (data.error) {
-                console.log('dataAPIStation ERROR', data);
+    callAPIStation(codeStation, dateTime) {
+        var result = "";
+
+        $.ajax({
+            url: this._APINativia + "stop_areas/stop_area"+codeStation+"/arrivals?from_datetime="+dateTime,
+        }).done($.proxy(function(data){
+
+            let res = data;
+            console.log('getAPIstation response', res);
+
+            // Boucle pour récup les infos des stations
+            for (var i = 0; i < data.arrivals.length; i++) {
+
+                result += '<p>' + data.arrivals[i].display_informations.commercial_mode + data.arrivals[i].display_informations.code +
+                    ', Sa destination est ' + data.arrivals[i].display_informations.direction + '</p></br>';
             }
-            else {
-                console.log('dataAPIStation response', data);
-            }
-        }, this));
+
+            $$('#test-page-content').html(result);
+
+        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
+            console.log('CALL API STATION FAILED: ', jqXHR, textStatus, errorThrown);
+        });
     }
 
     /**
@@ -55,15 +73,17 @@ class TestView {
      * @param codeStationTo
      * @param dateTime
      */
-    dataAPIItineraire(codeStationFrom,codeStationTo, dateTime) {
-        app.callAPIItineraire(codeStationFrom,codeStationTo,dateTime, $.proxy(function(data) {
-            if (data.error) {
-                console.log('dataAPIItineraire ERROR', data);
-            }
-            else {
-                console.log('dataAPIItineraire response', data);
-            }
-        }, this));
+    callAPIItineraire(codeStationFrom, codeStationTo, dateTime) {
+        $.ajax({
+            url: this._APINativia + "journeys?from=stop_area"+codeStationFrom+"&to=stop_area"+codeStationTo+"&datetime="+dateTime,
+        }).done($.proxy(function(data){
+
+            let res = data;
+            console.log('getAPIitinearaire response', res);
+
+        }, this)).fail(function( jqXHR, textStatus, errorThrown )  {
+            console.log('CALL API ITINERAIRE FAILED: ', jqXHR, textStatus, errorThrown);
+        });
     }
 
 }
