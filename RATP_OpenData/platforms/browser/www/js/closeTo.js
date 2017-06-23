8b4ddc2444da9342d95cff1ff78656097b581ef1
Hernandez Julien <julienhernandez14@gmail.com>
2017-06-23 11:02:20 +0200

    Ajout de la suppression des favoris + géolocalisation

--- RATP_OpenData/platforms/browser/www/js/closeTo.js
+++ RATP_OpenData/platforms/browser/www/js/closeTo.js
@@ -12,6 +12,44 @@ class CloseToView {
         console.log("closeTo:init()");
         console.log();
 
+        this.initMap();
     }
 
+    initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 16,
+            zoomControl : false,
+            scrollwheel : false,
+            draggable : false,
+            disableDoubleClickZoom : true,
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infoWindow.setPosition(pos);
+                infoWindow.setContent('Location found.');
+                map.setCenter(pos);
+            }, function() {
+                closeTo.handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            closeTo.handleLocationError(false, infoWindow, map.getCenter());
+        }
+    }
+
+    handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
 }
