0007a47aaf152af1ab7cd05ee889e96a737f5a04
LaurentPrz <laurent.perez.1992@gmail.com>
2017-06-26 17:33:01 +0200

    Fix Fontawesome icons

--- RATP_OpenData/www/js/application.js
+++ RATP_OpenData/www/js/application.js
@@ -11,7 +11,7 @@ class Application {
         this._serverPhonegap = location.href.split(':')[1].substr(2);
         this._user = "";
         this._APINativia = 'https://9a515a8c-7b22-456e-8e0d-6bdddfd9206f@api.navitia.io/v1/coverage/fr-idf/';
-        this._APIServer = 'http://192.168.56.1/server/';
+        this._APIServer = 'http://134.157.46.188/server/';
 
         // views
         this._test = new TestView();
