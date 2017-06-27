4921e6b78ff8ee3e98d28e1e47dfc58beb85ce80
Hernandez Julien <julienhernandez14@gmail.com>
2017-06-27 17:06:30 +0200

    utilisateur pas connect

--- RATP_OpenData/platforms/browser/www/js/account.js
+++ RATP_OpenData/platforms/browser/www/js/account.js
@@ -8,82 +8,81 @@ class AccountManager {
 
     }
 
-    init() {
+    init(id) {
         console.log("Account:init()");
 
         this.btnBack = $$('#btn-back-home');
-        this.btnBack.click($.proxy(this.restoreSwipePanel, this));
-
-        this.displayAccountList();
+        this.displayAccountList(id);
     }
 
     /**
      *  Affiche les comptes utilisateurs de la base de données mobiles
      */
-    displayAccountList(){
+    displayAccountList(id){
 
         let chaine = '<ul>',
             i = 0;
-        
-        if (app._accountList.length != 0) {
-            while (i < app._accountList.rows.length) {
-                chaine += '<li class="accordion-item">' +
-                    // '<a href="#" class="item-content item-link">' +
-                    // '<div class="item-inner">' +
-                    // '<div class="item-title">Vos comptes </div>' +
-                    // '</div>' +
-                    // '</a>' +
-                    '<div class="accordion-item-content">' +
-                    '<ul>';
-                while (app._accountList.rows[i]) {
-                    chaine += '<div class="list-block">' +
-                        '<li class="swipeout">' +
-                        '<div class="swipeout-content item-content">';
+        if (id != 0) {
+            if (app._accountList.length != 0) {
+                while (i < app._accountList.rows.length) {
+                    chaine += '<li class="accordion-item">' +
+                        // '<a href="#" class="item-content item-link">' +
+                        // '<div class="item-inner">' +
+                        // '<div class="item-title">Vos comptes </div>' +
+                        // '</div>' +
+                        // '</a>' +
+                        '<div class="accordion-item-content">' +
+                        '<ul>';
+                    while (app._accountList.rows[i]) {
+                        chaine += '<div class="list-block">' +
+                            '<li class="swipeout">' +
+                            '<div class="swipeout-content item-content">';
 
-                    if (app._accountList.rows[i].isDefault) {
+                        if (app._accountList.rows[i].isDefault) {
 
-                        // If the current account is the default account, expand its parent accordion block
-                        let pos = chaine.lastIndexOf('"accordion-item"') + 15,
-                            sliceLeft = chaine.slice(0, pos),
-                            sliceRight = chaine.slice(pos);
-                        chaine = sliceLeft + " accordion-item-expanded " + sliceRight;
+                            // If the current account is the default account, expand its parent accordion block
+                            let pos = chaine.lastIndexOf('"accordion-item"') + 15,
+                                sliceLeft = chaine.slice(0, pos),
+                                sliceRight = chaine.slice(pos);
+                            chaine = sliceLeft + " accordion-item-expanded " + sliceRight;
 
-                        // If the current account is the default account, add user accounts to the current server user list
-                        chaine += '<div class="item-media">' +
-                            '<i class="f7-icons icon-green">star_fill</i>' +
+                            // If the current account is the default account, add user accounts to the current server user list
+                            chaine += '<div class="item-media">' +
+                                '<i class="f7-icons icon-green">star_fill</i>' +
+                                '</div>' +
+                                '<div value="' + i + '"  class="item-inner" >' + app._accountList.rows[i].user + '</div>';
+                        } else {
+                            // Add user accounts to the current server user list
+                            chaine += '<div class="item-media">' +
+                                '</div>' +
+                                '<div value="' + i + '"  class="item-inner" >' + app._accountList.rows[i].user + '</div>';
+                        }
+
+                        // Add swipe-out action buttons to each user account item
+                        chaine += '</div>' +
+                            '<div class="swipeout-actions-right">' +
+                            '<a href="#" id="' + i + '-default" class="default action1 bg-orange">Par défaut</a>' +
+                            '<a href="#" id="' + i + '-delete" class="delete action2 bg-red">Supprimer</a>' +
                             '</div>' +
-                            '<div value="' + i + '"  class="item-inner" >' + app._accountList.rows[i].user + '</div>';
-                    } else {
-                        // Add user accounts to the current server user list
-                        chaine += '<div class="item-media">' +
-                            '</div>' +
-                            '<div value="' + i + '"  class="item-inner" >' + app._accountList.rows[i].user + '</div>';
+                            '</li>';
+                        i++;
                     }
-
-                    // Add swipe-out action buttons to each user account item
                     chaine += '</div>' +
-                        '<div class="swipeout-actions-right">' +
-                        '<a href="#" id="' + i + '-default" class="default action1 bg-orange">Par défaut</a>' +
-                        '<a href="#" id="' + i + '-delete" class="delete action2 bg-red">Supprimer</a>' +
-                        '</div>' +
-                        '</li>';
-                    i++;
+                        '</ul>' +
+                        '</div>';
                 }
-                chaine += '</div>' +
-                    '</ul>' +
-                    '</div>';
-            }
-            chaine += '</li>' +
-                '</ul>';
-            $$('#account-list').html(chaine);
+                chaine += '</li>' +
+                    '</ul>';
+                $$('#account-list').html(chaine);
 
-            this.btnDefault = $$('.default');
-            this.btnDefault.click($.proxy(this.defineFavorite, this));
-            this.btnDelete = $$('.delete');
-            this.btnDelete.click($.proxy(this.deleteAccount, this));
+                this.btnDefault = $$('.default');
+                this.btnDefault.click($.proxy(this.defineFavorite, this));
+                this.btnDelete = $$('.delete');
+                this.btnDelete.click($.proxy(this.deleteAccount, this));
+
+            }
 
         } else {
-
             chaine += '<div class="content-block favorites-error-connect"><div class="content-block-inner"><a href="#" class="back link">Veuillez vous connecter pour gérer les utilisateurs.</a></div></div>';
             $$('#account-list').html(chaine);
         }
